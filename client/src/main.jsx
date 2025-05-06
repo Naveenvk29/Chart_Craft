a6fd535416274d739d1bc54780a736da49fe03eb
Naveen Vinod Kumar <naveenvinodkumar29@gmail.com>
2025-05-06 15:00:10 +0530

    Implement user authentication pages: add LoginPage and SignUp components with Redux integration and routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,27 @@ import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import LoginPage from "./page/Auth/LoginPage.jsx";
+import SignUp from "./page/Auth/SignUp.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Route>
+  )
+);
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
   //  </StrictMode>
 );
